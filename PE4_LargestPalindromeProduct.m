3be82cc2243a0bf77ca62813ede95488ddc6589c
Dillon Allan <dlallan@users.noreply.github.com>
2017-12-22 13:36:56 -0700

    Change to arithmetic approach for PE4.

--- PE4_LargestPalindromeProduct.m
+++ PE4_LargestPalindromeProduct.m
@@ -19,12 +19,36 @@ function f = Main()
 end
 
 function TF = IsPalindrome(n)
-    s = num2str(n);
+    v = GetDigits(n);
+    s = length(v);
     TF = true;
-    for i=1:length(s)
-        if s(i)~=s(end-i+1)
-            TF=false;
-            break;
+    if s > 2        
+        for i=1:ceil(s/2)
+            if v(i)~=v(end-i+1)
+                TF=false;
+                break;
+            end
         end
     end
+end
+
+function s = NumSize(n)
+    s = max(ceil(log10(abs(n))),1);
+end
+
+function d = GetDigits(n)
+    s = NumSize(n);
+    x = abs(n);
+    d = zeros(s,1);
+    % get largest digit
+    d(1) = floor(x/10^(s-1));
+    % get middle digits
+    for i=2:length(d)
+        y = x;
+        % subtract previous digits
+        for j=1:i
+            y=y-d(j)*10^(s-j);
+        end
+        d(i)=floor(y/10^(s-i));
+    end    
 end
